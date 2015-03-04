0f73b36269786feed206e1c414522a2dd05ec93c
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-03-04 10:09:30 +0800

    upgrade clojure and lein-cljsbuild version
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :plugins [[lein-cljsbuild "1.0.4"]]
+  :plugins [[lein-cljsbuild "1.0.5"]]
 
   :cljsbuild {
               :builds [{
@@ -14,4 +14,4 @@
                                    :target :nodejs
                                    :optimizations :simple
                                    :pretty-print false}}]}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
